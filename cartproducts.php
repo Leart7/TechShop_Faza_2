bcb84d5cbe7a1bd359fa01eb56acdd72d5ea7b30
Leart Shabani <leartshabani77@gmail.com>
2023-03-05 01:09:09 +0100

    second change

--- cartproducts.php
+++ cartproducts.php
@@ -11,7 +11,7 @@
   if(isset($_SESSION['perdoruesi'])){
     $perdoruesi = $_SESSION['perdoruesi']['email'];
   }
-        
+        //second change comment
        
   $carts = merrCart();
   $numriCart = numberofCarts($perdoruesi);
