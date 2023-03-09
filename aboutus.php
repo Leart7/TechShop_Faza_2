b80018d7e1c0cce8e04271cd09e000c86026a7bc
Leart Shabani <leartshabani77@gmail.com>
2023-03-09 01:34:05 +0100

    news/about us page refreshed,modified

--- aboutus.php
+++ aboutus.php
@@ -8,6 +8,17 @@
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"/>
 
 <body>
+  <div class="main-section">
+    
+    <div class="main-text">
+    <img src="images/Apple-Logo.png" id="apple" alt="">
+      <h1>Official Apple Reseller</h1>
+      <h3>Find the newest apple products in our shop</h3>
+      <button id="learnmore">Explore</button>
+    </div>
+    
+
+  </div>
   <div class="containerslider">
     <div class="swiper">
       <!-- Additional required wrapper -->
@@ -50,5 +61,44 @@
 
 });
   </script>
+
+  <div class="threecontainer">
+    <div class="threecontainerrow">
+      <div class="vertaligned">
+      <div class="imgcontainer">
+      <img src="images/delivery.png" id="deliverytruck" alt="">
+      </div>
+      <div class="textcontainer">
+      <h3>FREE SHIPPING</h3><br>
+      <p>Free Shipping in Kosovo for orders over 10$</p>
+      </div>
+      </div>
+    
+    </div>
+    <div class="threecontainerrow">
+      <div class="vertaligned">
+        <div class="imgcontainer">
+        <img src="images/PngItem_2797797.png" id="paymentmethod" alt="">
+        </div>
+        <div class="textcontainer">
+        <h3>100% Payment Secure</h3>
+        <p>Pay with credit card or by cash</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="threecontainerrow">
+      <div class="vertaligned">
+      <div class="imgcontainer">
+      <img src="images/return.png" id="returnicon" alt="">
+      </div>
+      <div class="textcontainer">
+      <h3>7 Days Return</h3>
+      <p>Simply return it within 7 days for an exchange</p>
+      </div>
+      </div>
+     
+    </div>
+  </div>
 
 
